2af4c126887626f68797d83198a0ceb99b809755
BonuRakesh <112648987+BonuRakesh@users.noreply.github.com>
2022-09-06 11:15:27 +0530

    Add files via upload

---
+++ pascals triangle as triangle.py
@@ -0,0 +1,13 @@
+def solve(n):
+   for i in range(n+1):
+      for j in range(n-i):
+         print(' ', end='')
+
+      C = 1
+      for j in range(1, i+1):
+         print(C, ' ', sep='', end='')
+         C = C * (i - j) // j
+      print()
+
+n = 5
+solve(n)
